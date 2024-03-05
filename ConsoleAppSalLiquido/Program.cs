1e1a52fe54d1847aecf991f53b3d8b2e3b291502
Usuario <Usuario@CPD02-M06>
2024-03-05 10:35:22 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppSalLiquido/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSalLiquido
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salário bruto: ");
+            int bruto = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vale alimentação: ");
+            int ali = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Número de filhos: ");
+            int filho = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor por filho: ");
+            int valfilho = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Número de horas extras: ");
+            int hora = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor por hora extra: ");
+            int valhora = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor do INSS: ");
+            int inss = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor do IRPF: ");
+            int irpf = int.Parse(Console.ReadLine());
+
+            double salliquido = bruto + ali + (filho * valfilho) + (hora * valhora) - inss - irpf;
+
+            Console.WriteLine("O salário líquido do/a funcionário/a é de: R$" + salliquido);
+            Console.ReadKey();
+
+        }
+    }
+}
